0942b2298ee8438d5ba081c721c8b00e1eccaba0
Matthew Foyle <matt@moltin.com>
2018-03-17 10:16:43 -0700

    - Set up env for FaaS - Add libraries for manual API calls - Add mock data in a separate file

---
+++ utils/mock.js
@@ -0,0 +1,24 @@
+var exports = module.exports = {};
+
+exports.customer = {
+    name: 'Jon Doe',
+    email: 'jon.doe@gmail.com',
+};
+
+exports.address = {
+    first_name: 'Jon',
+    last_name: 'Doe',
+    line_1: '123 Sunny Street',
+    line_2: 'Sunnycreek',
+    city: 'Sunnyvale',
+    county: 'California',
+    country: 'US',
+    postcode: 'CA94040',
+};
+
+exports.card = {
+    number: '4242424242424242',
+    month: '02',
+    year: '2020',
+    verification_value: '123',
+}
